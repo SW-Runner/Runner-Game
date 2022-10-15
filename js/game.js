4c65a350eb3a5b015d3d6760652567ca7ae5a02a
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-10-15 16:44:31 +0900

    캐릭터 모델 추가, 배경 추가, 장애물 추가해야함

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+// 카메라 위치 설정
+// z 값이 커질 수록, 모니터에 가까워짐,
+//
 let cameraX = 0;
 let cameraY = 1500;
 let cameraZ = -1000;
@@ -7,6 +10,22 @@ let runningAction;
 
 let clock = new THREE.Clock();
 let mixer;
+
+let Colors = {
+    cherry: 0xe35d6a,
+    blue: 0x1560bd,
+    white: 0xd8d0d1,
+    black: 0x000000,
+    brown: 0x654321,
+    peach: 0xffdab9,
+    yellow: 0xffff00,
+    olive: 0x556b2f,
+    grey: 0x696969,
+    sand: 0x835c3b,
+    brownDark: 0x23190f,
+    green: 0x669900,
+    grass: 0xE5D85C
+};
 window.onload = function init() {
     // HTML world랑 js 연결하기
     let world = document.getElementById('world');
@@ -29,7 +48,7 @@ window.onload = function init() {
     let camera = new THREE.PerspectiveCamera(
         60, world.clientWidth / world.clientHeight, 1, 120000);
 
-    //camera.position.set(0, 1500, -2000);
+    //camera.position.set(0, 1500, -1000);
     camera.position.set(cameraX, cameraY, cameraZ);
     camera.lookAt(new THREE.Vector3(0, 600, -5000));
     window.camera = camera;
@@ -38,10 +57,15 @@ window.onload = function init() {
     let light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
     scene.add(light);
 
+
+    // 캐릭터 렌더링하기
+    // TODO: 무슨 캐릭터 쓸지 결정하기, character, mei, moon_walk, walking_robot 중 하나 선정해야된다
     const loader = new THREE.GLTFLoader();
-    loader.load('./character/scene.gltf', function(gltf){
+    loader.load('./mei/scene.gltf', function(gltf){
         running = gltf.scene.children[0];
-        running.scale.set(1,1,1);
+        // 캐릭터 크기 설정
+        running.scale.set(5,-5,5);
+        // 캐릭터 위치 설정
         running.position.set(0,0,-4000);
         scene.add(gltf.scene);
         runningCharacter = running;
@@ -52,11 +76,42 @@ window.onload = function init() {
         console.error(error);
     });
 
+    // ground 설정하기
+    let ground = createGround(3000, 20, 120000, Colors.olive, 0, -400, -60000);
+    scene.add(ground);
+    // 장애물 & 오브젝트 만들기
+    // TODO: 장애물 어떻게 만들어질지 정해야될듯
+
     animate();
+
+
     function animate() {
-        var delta = clock.getDelta();
+        let delta = clock.getDelta();
         if ( mixer ) mixer.update( delta );
         renderer.render(scene,camera);
         requestAnimationFrame(animate);
     }
+
+    function createGround(dx, dy, dz, map, x, y, z, notFlatShading) {
+
+        const loader = new THREE.TextureLoader();
+
+        const materials = [
+            new THREE.MeshBasicMaterial({map: loader.load('https://images.pexels.com/photos/13644281/pexels-photo-13644281.jpeg')}),
+            new THREE.MeshBasicMaterial({map: loader.load('https://images.pexels.com/photos/13644281/pexels-photo-13644281.jpeg')}),
+            new THREE.MeshBasicMaterial({map: loader.load('https://images.pexels.com/photos/13644281/pexels-photo-13644281.jpeg')}),
+            new THREE.MeshBasicMaterial({map: loader.load('https://images.pexels.com/photos/13644281/pexels-photo-13644281.jpeg')}),
+
+        ];
+
+        var geom = new THREE.BoxGeometry(dx, dy, dz);
+
+        var box = new THREE.Mesh(geom, materials);
+
+        box.castShadow = true;
+        box.receiveShadow = true;
+        box.position.set(x, y, z);
+        return box;
+    }
+
 };
